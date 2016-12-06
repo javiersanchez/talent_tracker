b5065e3b07ec09631d6a8245d24b1d3359617ad5
Your Name <jsanchez@occ.com.mx>
2016-12-05 23:23:05 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express')
+var app = express()
+
+/*app.get('/', function (req, res) {
+  res.send('Hello World!')
+})*/
+
+app.get('/login', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/login', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/search', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/search', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/search_result', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/search_result', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/create', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/create', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/edit/:id', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/edit/:id', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/view/:id', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/view/:id', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
