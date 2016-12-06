4e0ab553f4ea02f621e80135fd3d8b8237584437
Marco Gallen <mgallen@OCCQRO.local>
2016-12-06 00:03:02 -0600

    Se agrego la libreria de express-handlebars y carpetas para la vista y el layout

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 /*app.get('/', function (req, res) {
   res.send('Hello World!')
 })*/
 
 app.get('/login', function (req, res) {
-  res.send('Hello World!')
+  res.render('login');
 })
 
 app.post('/login', function (req, res) {
