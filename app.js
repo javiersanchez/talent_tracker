978963936274a43d4421af12909dded2a4696edd
Marco Gallen <mgallen@OCCQRO.local>
2016-12-06 10:34:31 -0600

    Se agregaron archivos de CSS y JS y la ruta estatica para los archivos publicos

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
 
+
+//Llamamos Handlebars para usarlo en el proyecto y asignamos un layout (default)
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 
+
+//Asignación de HB como engine de rendereo
 app.engine('handlebars', handlebars.engine);
+
+//View set 
 app.set('view engine', 'handlebars');
 /*app.get('/', function (req, res) {
   res.send('Hello World!')
 })*/
+
+//Asignar ruta publica pra elementos estaticos
+app.use(express.static(__dirname + "/public"));
+
 
 app.get('/login', function (req, res) {
   res.render('login');
